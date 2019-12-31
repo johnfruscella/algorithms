51d25081ed032a419f1bcd998dfa3fbbff100159
johnfruscella <johnfruscella@gmail.com>
2019-12-30 22:00:58 -0500

    spinaltapcase solution

--- spinaltapcase.js
+++ spinaltapcase.js
@@ -1,7 +1,10 @@
 function spinalCase(str) {
-   str = str.toLowerCase().replace(/\s/g,"-");
-   console.log(str);
+    str = str.replace(/([a-z])([A-Z])/g, '$1 $2').toLowerCase().replace(/\_|\W/g, "-")
+    console.log(str);
     return str;
-  }
-  
-  spinalCase('This Is Spinal Tap');
+}
+
+spinalCase('This Is Spinal Tap');
+spinalCase("The_Andy_Griffith_Show");
+spinalCase("thisIsSpinalTap");
+spinalCase("AllThe-small Things");
