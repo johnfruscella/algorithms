e4c4693a20bf6d82fcd0d15ea8dd8cc9f3d4550a
johnfruscella <johnfruscella@gmail.com>
2020-01-03 00:05:56 -0500

    pig latin v 2

--- piglatin.js
+++ piglatin.js
@@ -1,6 +1,6 @@
 function translatePigLatin(str) {
 
-    let firstPosition = findFirstVowelPosition(str);
+    var firstPosition = findFirstVowelPosition(str);
     if (firstPosition > 0) {
         return str.slice(firstPosition) + str.slice(0, firstPosition) + "ay";
 
@@ -10,8 +10,8 @@ function translatePigLatin(str) {
   
 
   function findFirstVowelPosition(str) {
-      for (let i = 0; i > str.length; i++) {
-          if ('aeiou'.indexOf(str[i] !== -1)){
+      for (let i = 0; i < str.length; i++) {
+          if ("aeiou".indexOf(str[i]) !== -1){
               return i;
           }
       }
