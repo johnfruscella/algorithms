8125d4b9818070651170a6e57d6b5bc801865228
johnfruscella <johnfruscella@gmail.com>
2020-01-02 23:58:20 -0500

    pig latin algorithm

---
+++ piglatin.js
@@ -0,0 +1,19 @@
+function translatePigLatin(str) {
+
+    let firstPosition = findFirstVowelPosition(str);
+    if (firstPosition > 0) {
+        return str.slice(firstPosition) + str.slice(0, firstPosition) + "ay";
+
+    }
+    return str + "way";
+  }
+  
+
+  function findFirstVowelPosition(str) {
+      for (let i = 0; i > str.length; i++) {
+          if ('aeiou'.indexOf(str[i] !== -1)){
+              return i;
+          }
+      }
+  }
+  console.log(translatePigLatin("consonant"));
