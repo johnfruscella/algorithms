e4f9440bc8d26f7286a6894aabb491355fae1b28
johnfruscella <johnfruscella@gmail.com>
2020-01-03 02:26:04 -0500

    pig latin solved formatted

--- piglatin.js
+++ piglatin.js
@@ -1,16 +1,16 @@
 function translatePigLatin(str) {
-    let vowels= ["a","e","i","o","u"]
-    let index=0;
-    
-     if(vowels.indexOf(str[index])!== -1){ 
-         return str+"way";
-     }
-     for(index=1;index<str.length;index++){
-         if(vowels.indexOf(str[index]) !==-1){
-             break;
-        }
-     }
-    return str.slice(index,str.length)+str.slice(0, index)+ "ay";
-    }
+    let vowels = ["a", "e", "i", "o", "u"]
+    let index = 0;
 
-    console.log(translatePigLatin("rhythm"));
+    if (vowels.indexOf(str[index]) !== -1) {
+        return str + "way";
+    }
+    for (index = 1; index < str.length; index++) {
+        if (vowels.indexOf(str[index]) !== -1) {
+            break;
+        }
+    }
+    return str.slice(index, str.length) + str.slice(0, index) + "ay";
+}
+
+console.log(translatePigLatin("rhythm"));
