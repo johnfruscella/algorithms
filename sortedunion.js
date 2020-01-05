c33600e710cc992b91a7f0c4268c56b1bd5d2ee1
johnfruscella <johnfruscella@gmail.com>
2020-01-05 01:26:25 -0500

    sorted union algorithm solved

--- sortedunion.js
+++ sortedunion.js
@@ -1,17 +1,23 @@
-function uniteUnique(arr) {
-    console.log(arguments.length);
-    let result = arguments[0];
-    for (let i  = 1; i < arguments.length; i++) {
-         for (let j = 0; j < arguments[i].length; j++) {
-           if (result.indexOf(arguments[i][j]) < 0) {
-               result.push(arguments[i][j]);
-         }
-      }
-      console.log(result);
-      return result;	
-    }
- }
+function uniteUnique() {
+  var array = [],
+      finalArray = [];
+  for(let i = 0; i < arguments.length; i++){
+      array.push(arguments[i]);
+  }
 
- //uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
- //uniteUnique([1, 2, 3], [5, 2, 1]);
+  for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array[i].length; j++) {
+          if (!finalArray.includes(array[i][j])) {  
+              finalArray.push(array[i][j]);
+
+          }
+      }
+  }
+
+  console.log(finalArray);
+  return finalArray;
+}
+
+ uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+ uniteUnique([1, 2, 3], [5, 2, 1]);
  uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]); //should return [1, 2, 3, 5, 4, 6, 7, 8]
