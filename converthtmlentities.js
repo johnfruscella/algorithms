836efca673a002f35c5d8dab92bb282447ac23c8
johnfruscella <johnfruscella@gmail.com>
2020-01-05 19:10:01 -0500

    convert html entities algorithm solved

--- converthtmlentities.js
+++ converthtmlentities.js
@@ -3,26 +3,26 @@
 
 
 function convertHTML(str) {
-    for (let i =0; i< str.length; i++) {
+    for (let i = 0; i < str.length; i++) {
         switch (str[i]) {
-            case '&': str = str.replace('&','&amp;');
-            break;
-            case '<': str = str.replace('<','&lt;');
-            break;
-            case '>': str = str.replace('>','&gt;');
-            break;
-            case `"`:  str = str.replace('"','&quot;');
-            break;
-            case `'`: str = str.replace("'",'&apos;')
-            break; 
-            
+            case '&': str = str.replace('&', '&amp;');
+                break;
+            case '<': str = str.replace('<', '&lt;');
+                break;
+            case '>': str = str.replace('>', '&gt;');
+                break;
+            case `"`: str = str.replace('"', '&quot;');
+                break;
+            case `'`: str = str.replace("'", '&apos;')
+                break;
+
         }
-        
-        }
+
+    }
     console.log(str);
     return str;
-  }
-  
-  convertHTML("Dolce & Gabbana"); //should return Dolce &amp; Gabbana
-  convertHTML("Hamburgers < Pizza < Tacos") //should return Hamburgers &lt; Pizza &lt; Tacos
-  convertHTML("abc") //should return abc
+}
+
+convertHTML("Dolce & Gabbana"); //should return Dolce &amp; Gabbana
+convertHTML("Hamburgers < Pizza < Tacos") //should return Hamburgers &lt; Pizza &lt; Tacos
+convertHTML("abc") //should return abc
