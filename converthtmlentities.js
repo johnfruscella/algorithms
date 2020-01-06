d9440f0ab9753b56fad5efd78d90d65cdb83d651
johnfruscella <johnfruscella@gmail.com>
2020-01-05 20:21:49 -0500

    cleaned up code

--- converthtmlentities.js
+++ converthtmlentities.js
@@ -5,15 +5,15 @@
 function convertHTML(str) {
     for (let i = 0; i < str.length; i++) {
         switch (str[i]) {
-            case '&': str = str.replace('&', '&amp;');
+            case '&': str.replace('&','&amp;');
                 break;
-            case '<': str = str.replace('<', '&lt;');
+            case '<': str.replace('<', '&lt;');
                 break;
-            case '>': str = str.replace('>', '&gt;');
+            case '>': str.replace('>', '&gt;');
                 break;
-            case `"`: str = str.replace('"', '&quot;');
+            case `"`: str.replace('"', '&quot;');
                 break;
-            case `'`: str = str.replace("'", '&apos;')
+            case `'`: str.replace("'", '&apos;')
                 break;
 
         }
