6039c0ac811854cc134e310780ffc00e5ada6d5f
johnfruscella <johnfruscella@gmail.com>
2020-01-20 23:56:38 -0500

    everything be true algorithm solved

---
+++ everythingbetrue.js
@@ -0,0 +1,17 @@
+function truthCheck(collection, pre) {
+  return collection.every(prop => {
+    return prop[pre];
+  });
+}
+
+console.log(
+  truthCheck(
+    [
+      { user: "Tinky-Winky", sex: "male" },
+      { user: "Dipsy", sex: "male" },
+      { user: "Laa-Laa", sex: "female" },
+      { user: "Po", sex: "female" }
+    ],
+    "sex"
+  )
+); //should return true
