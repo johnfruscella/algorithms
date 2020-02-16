dedd67f3ebbc5ee859952626e8e4b784fe3e1d3e
johnfruscella <johnfruscella@gmail.com>
2020-02-16 00:02:04 -0500

    corrected solution for map debris algorithm

--- mapDebris.js
+++ mapDebris.js
@@ -1,9 +1,10 @@
 function orbitalPeriod(arr) {
     let GM = 398600.4418;
-    let earthRadius = 9367.4447;
-    
-    arr.forEach(function(object) {
-        object.orbitalPeriod = Math.round(2*Math.PI*Math.sqrt(Math.pow(earthRadius + object.avgAlt, 3)/GM));
-    delete object.avgAlt;});
+    let earthRadius = 6367.4447;
+  
+    arr.forEach(function (object) {
+      object.orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earthRadius + object.avgAlt, 3) / GM));
+      delete object.avgAlt;
+    });
     return arr;
-    }
+  }
