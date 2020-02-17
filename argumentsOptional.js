af94e0fc4edee828879110065e0b62d0332f7f89
johnfruscella <johnfruscella@gmail.com>
2020-02-16 20:34:54 -0500

    arguments optional algorithm

--- argumentsOptional.js
+++ argumentsOptional.js
@@ -1,12 +1,34 @@
 function addTogether() {
-	var args = Array.from(arguments);
-	return args.some(n => typeof n !== 'number') ? 	undefined:
-	args.length> 1 ?
-	args.reduce((acc, n) => acc += n, 0):
-	(n) => typeof n === 'number' ?
-	n + args[0]:
-	undefined
-}
+
+	let checkNum = function(num) {
+	  if (typeof num !== "number") {
+		return undefined;
+	  } else return num;
+	};
+  
+	if (arguments.length > 1) {
+	  let a = checkNum(arguments[0]);
+	  let b = checkNum(arguments[1]);
+	  if (a === undefined || b === undefined) {
+		return undefined;
+	  } else {
+		return a + b;
+	  }
+	} else {
+	  let c = arguments[0];
+
+	  if (checkNum(c)) {
+		return function(arg2) {
+		  if (c === undefined || checkNum(arg2) === undefined) {
+			return undefined;
+		  } else {
+			return c + arg2;
+		  }
+		};
+	  }
+	}
+  }
+  
 
 console.log(addTogether(2,3));
 console.log(addTogether(2)(3));
